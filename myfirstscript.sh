c22350da8cb8a86dd1f1ce95b6842bb67b565499
abidino1 <abidino@gmail.com>
2023-11-27 13:15:08 -0800

    installing apache2/mysql/postgresql

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,5 +1,5 @@
- This is a test
+# This is a test
 sudo apt install apache2 -y
 sudo apt install mysql-server -y
-sudo apt install postgresql-server-dev-all
+sudo apt install postgresql-server-dev-all -y
 
